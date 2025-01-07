73e51addbaafb5d82406ffbf6ad87d62c7205d4c
aliahasan <aanabin18@gmail.com>
2025-01-07 21:36:34 +0600

    updated

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,22 +1,30 @@
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Layout, Menu } from "antd";
-import { createElement } from "react";
-import { Outlet } from "react-router-dom";
+import { Layout, Menu, MenuProps } from "antd";
+import { NavLink, Outlet } from "react-router-dom";
 const { Header, Content, Footer, Sider } = Layout;
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const items: MenuProps["items"] = [
+  {
+    key: "Dashboard",
+    label: <NavLink to="/admin">DashBoard</NavLink>,
+  },
+  {
+    key: "User Management",
+    label: "User Management",
+    children: [
+      {
+        key: "create-admin",
+        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
+      },
+      {
+        key: "create-faculty",
+        label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
+      },
+      {
+        key: "create-student",
+        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
+      },
+    ],
+  },
+];
 const MainLayout = () => {
   return (
     <Layout style={{ height: "100vh" }}>
